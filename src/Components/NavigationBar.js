70c745927ccb54578a4f30bb8aa3a13fb782e71e
shidqiiii <38581836+shidqiiii@users.noreply.github.com>
2022-06-09 19:45:18 +0700

    Update NavigationBar.js

--- src/Components/NavigationBar.js
+++ src/Components/NavigationBar.js
@@ -5,13 +5,16 @@ import { Link } from 'react-router-dom'
 export default function NavigationBar() {
     return (
         <>
-            <Navbar bg="dark" variant="dark">
+            <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
                 <Container>
                     <Navbar.Brand as={Link} to="/">ToDo List</Navbar.Brand>
-                    <Nav className="ms-auto">
-                        <Nav.Link as={Link} to="/">Home</Nav.Link>
-                        <Nav.Link as={Link} to="/todo">TodoList</Nav.Link>
-                    </Nav>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="ms-auto">
+                            <Nav.Link as={Link} to="/">Home</Nav.Link>
+                            <Nav.Link as={Link} to="/todo">TodoList</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
                 </Container>
             </Navbar>
         </>
